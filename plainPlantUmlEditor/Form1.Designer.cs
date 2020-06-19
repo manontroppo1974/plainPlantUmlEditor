a33c0d2ab65e98241487612ebd1c15b93954abf8
manontroppo1974 <ma.non.troppo.1974@gmail.com>
2020-06-19 10:31:47 +0900

    first commit

---
+++ plainPlantUmlEditor/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace plainPlantUmlEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(371, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(3, 588);
+            this.splitter1.TabIndex = 1;
+            this.splitter1.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(374, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(577, 588);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.Black;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.richTextBox1.Font = new System.Drawing.Font("ＭＳ ゴシック", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(255)))), ((int)(((byte)(244)))));
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(371, 588);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(951, 588);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.richTextBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "PlainPlantPumlEditor";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ plainPlantUmlEditor/Form1.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Reflection;
+
+namespace plainPlantUmlEditor
+{
+    public partial class Form1 : Form
+    {
+        string curFile = null;
+        string curDir = null;
+
+        private const uint IMF_DUALFONT = 0x80;
+        private const uint WM_USER = 0x0400;
+        private const uint EM_SETLANGOPTIONS = WM_USER + 120;
+        private const uint EM_GETLANGOPTIONS = WM_USER + 121;
+
+        [System.Runtime.InteropServices.DllImport("USER32.dll")]
+        private static extern uint SendMessage(
+            System.IntPtr hWnd,
+            uint msg,
+            uint wParam,
+            uint lParam); 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            myDragDrop(sender, e);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            myDragEnter(sender, e);
+        }
+
+        private void richTextBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            myDragDrop(sender, e);
+        }
+
+        private void richTextBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            myDragEnter(sender, e);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // ctrl+S
+            if (e.KeyCode == Keys.S && e.Control == true)
+            {
+                Console.WriteLine(curFile);
+
+                if (curFile == null || e.Shift == true)
+                {
+                    //ファイル保存ダイアログを表示する
+                    SaveFileDialog sfd = new SaveFileDialog();
+                    sfd.FileName = "";
+                    sfd.InitialDirectory = curDir;
+                    sfd.Filter = "plantUMLファイル(*.puml)|*.puml|すべてのファイル(*.*)|*.*";
+                    sfd.FilterIndex = 1;
+                    sfd.Title = "保存先のファイルを選択してください";
+                    sfd.RestoreDirectory = true;
+                    sfd.OverwritePrompt = true;
+                    sfd.CheckPathExists = true;
+
+                    if (sfd.ShowDialog() == DialogResult.OK)
+                    {
+                        curFile = sfd.FileName;
+                        curDir = System.IO.Path.GetDirectoryName(curFile);
+                        Console.WriteLine(curFile);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Save cancel.");
+                        return;
+                    }
+                }
+
+                using (StreamWriter sw = new StreamWriter(curFile, false, Encoding.UTF8))
+                {
+                    //sw.Write(textBox1.Text);
+                    sw.Write(richTextBox1.Text);
+                }
+
+                this.Text = curFile;
+                doPlantUml();
+            }
+
+            // ctrl+o
+            else if(e.KeyCode == Keys.O && e.Control == true)
+            {
+                //OpenFileDialogを表示する
+                OpenFileDialog ofd = new OpenFileDialog();
+                ofd.FileName = "";
+                ofd.InitialDirectory = curDir;
+                ofd.Filter = "plantUMLファイル(*.puml)|*.puml|すべてのファイル(*.*)|*.*";
+                ofd.FilterIndex = 1;
+                ofd.Title = "開くファイルを選択してください";
+                ofd.RestoreDirectory = true;
+                ofd.CheckFileExists = true;
+                ofd.CheckPathExists = true;
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    /*
+                    curFile = ofd.FileName;
+                    curDir = System.IO.Path.GetDirectoryName(curFile);
+                    Console.WriteLine(curFile);
+                    */
+                    openFile(ofd.FileName);
+                }
+                else
+                {
+                    Console.WriteLine("Open cancel.");
+                    return;
+                }
+            }
+
+            // ctrl+c
+            else if (e.KeyCode == Keys.C && e.Control == true)
+            {
+                Console.WriteLine("Clipboard.SetImage: pictureBox1.Image");
+                Clipboard.SetImage(pictureBox1.Image);
+            }
+
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            myDragDrop(sender, e);
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            myDragEnter(sender, e);
+        }
+
+        private void myDragEnter(object sender, DragEventArgs e)
+        {
+            //ファイルがドラッグされている場合、カーソルを変更する
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+
+        private void myDragDrop(object sender, DragEventArgs e)
+        {
+            string[] fileName = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            if (fileName.Length <= 0)
+            {
+                return;
+            }
+
+            Console.WriteLine(fileName[0]);
+
+            /*
+            if (!File.Exists(fileName[0])) return;
+
+            using (StreamReader sr = new StreamReader(fileName[0], System.Text.Encoding.UTF8))
+            {
+                string txt = sr.ReadToEnd();
+                textBox1.Text = txt;
+                curFile = fileName[0];
+                curDir = System.IO.Path.GetDirectoryName(curFile);
+                this.Text = curFile;
+            }
+            */
+            openFile(fileName[0]);
+
+            //doPlantUml();
+        }
+
+        private void openFile(string fileName)
+        {
+            if (!File.Exists(fileName)) return;
+
+            using (StreamReader sr = new StreamReader(fileName, System.Text.Encoding.UTF8))
+            {
+                string txt = sr.ReadToEnd();
+                //textBox1.Text = txt;
+                richTextBox1.Text = txt;
+                curFile = fileName;
+                curDir = System.IO.Path.GetDirectoryName(curFile);
+                this.Text = curFile;
+            }
+
+            doPlantUml();
+        }
+
+        private void doPlantUml()
+        {
+            Assembly myAssembly = Assembly.GetEntryAssembly();
+            string appFile = myAssembly.Location;
+            string appDir = System.IO.Path.GetDirectoryName(appFile);
+
+            string pngFile = System.IO.Path.ChangeExtension(curFile, "png");
+            DateTime lastTimeCur = File.GetLastWriteTime(curFile);
+            DateTime lastTimePng = DateTime.MinValue;
+            if (File.Exists(pngFile))
+            {
+                lastTimePng = File.GetLastWriteTime(pngFile);
+            }
+            Console.WriteLine(curFile + ": " + lastTimeCur);
+            Console.WriteLine(pngFile + ": " + lastTimePng);
+
+            if (lastTimeCur > lastTimePng)
+            {
+                System.Diagnostics.Process pro = new System.Diagnostics.Process();
+                pro.StartInfo.FileName = "java";            // コマンド名
+                pro.StartInfo.Arguments = @"-jar """ + appDir + @"\plantuml.jar"" -charset utf-8 """ + curFile + @"""";               // 引数
+                pro.StartInfo.CreateNoWindow = true;            // DOSプロンプトの黒い画面を非表示
+                pro.StartInfo.UseShellExecute = false;          // プロセスを新しいウィンドウで起動するか否か
+                pro.StartInfo.RedirectStandardOutput = true;    // 標準出力をリダイレクトして取得したい
+                pro.StartInfo.RedirectStandardError = true;
+                Console.WriteLine(pro.StartInfo.Arguments);
+
+                pro.Start();
+
+                string so = pro.StandardOutput.ReadToEnd();
+                string se = pro.StandardError.ReadToEnd();
+                Console.WriteLine(so);
+                Console.WriteLine(se);
+            }
+
+            if (File.Exists(pngFile))
+            {
+                pictureBox1.ImageLocation = pngFile;
+                pictureBox1.Refresh();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Size = Properties.Settings.Default.formSize;
+            this.Location = Properties.Settings.Default.formPosition;
+            //this.textBox1.Size = Properties.Settings.Default.textBoxSize;
+            this.richTextBox1.Size = Properties.Settings.Default.textBoxSize;
+            this.curDir = Properties.Settings.Default.lastDir;
+
+            this.richTextBox1.AllowDrop = true;
+            this.richTextBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.richTextBox1_DragEnter);
+            this.richTextBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.richTextBox1_DragDrop);
+            richTextBox1.LanguageOption = RichTextBoxLanguageOptions.UIFonts;
+
+            // リッチテキストボックスのフォントが勝手に変わるのを抑制
+            uint lParam;
+            lParam = SendMessage(richTextBox1.Handle, EM_GETLANGOPTIONS, 0, 0);
+            lParam &= ~IMF_DUALFONT;
+            SendMessage(richTextBox1.Handle, EM_SETLANGOPTIONS, 0, lParam);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.formSize = this.Size;
+            Properties.Settings.Default.formPosition = this.Location;
+            //Properties.Settings.Default.textBoxSize = this.textBox1.Size;
+            Properties.Settings.Default.textBoxSize = this.richTextBox1.Size;
+            Properties.Settings.Default.lastDir = this.curDir;
+
+            Properties.Settings.Default.Save();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            var args = Environment.GetCommandLineArgs();
+            if (args.Length >= 2)
+            {
+                //textBoxArgs.Text = args.Skip(1).Aggregate((a, b) => a + b);
+                if (args.Count() >= 2 /*&& Path.GetExtension(args[1]).ToLower() == ".puml"*/)
+                {
+                    openFile(args[1]);
+                }
+            }
+        }
+    }
+}
